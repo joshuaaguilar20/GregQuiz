639fd4b7f9276fd7b8d37079faf336cc61e42038
Joshua-Aguilar <joshuaaguilar20@gmail.com>
2018-09-18 14:18:33 -0700

    first try

---
+++ src/api/quizQuestions.js
@@ -0,0 +1,101 @@
+var quizQuestions = [
+  {
+      question: "'Sur le plan professionnel vous êtes plutôt :'",
+      answers: [
+          {
+              type: "r",
+              content: "Ambitieux"
+          },
+          {
+              type: "j",
+              content: "Plein d\'idées'"
+          },
+          {
+              type: "v",
+              content: "Méthodique"
+          },
+          {
+            type: "b",
+            content: "Pointilleux'"
+        }
+      ]
+  },
+  {
+      question: "Sur le lieu de votre travail ce qui est important pour vous :'",
+      answers: [
+          {
+              type: "b",
+              content: "Réaliser ma mission avec perfection"
+          },
+          {
+              type: 'v',
+              content: "'L\'harmonie dans l\'équipe"
+          },
+          {
+              type: "'j',",
+              content: "Les relations avec les clients'"
+          }
+      ]
+  },
+  {
+      question: "'On dit de vous que vous êtes :'",
+      answers: [
+          {
+              type: "v",
+              content: "Serviable"
+          },
+          {
+              type: "b",
+              content: "Discipline"
+          },
+          {
+              type: "j",
+              content: "Convainquant"
+          },
+          {
+            type: "r",
+            content: "Determine"
+        }
+      ]
+  },
+  {
+      question: "'Vous êtes de nature :'",
+      answers: [
+          {
+              type: "j",
+              content: 'Joviale'
+          },
+          {
+              type: "r",
+              content: "Impatiente"
+          },
+          {
+              type:'b',
+              content: "Perfectionniste"
+          },
+          {
+            type:'v',
+            content: "Calme"
+        },
+      ]
+  },
+  {
+      question: "Vous avez peur :'",
+      answers: [
+          {
+              type: "v",
+              content: "'Des conflits'"
+          },
+          {
+              type:'b',
+              content: 'Des critiques'
+          },
+          {
+              type: 'j',
+              content: "'D\'être seul'"
+          }
+      ]
+  }
+];
+
+export default quizQuestions;
